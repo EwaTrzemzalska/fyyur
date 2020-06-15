071b1e365f0b3941c6d1f3adc52695dcbe09b306
Ewa Trzemżalska <trzemson@gmail.com>
2020-06-15 17:36:04 +0200

    Add artists listing

--- app.py
+++ app.py
@@ -307,17 +307,14 @@ def delete_venue(venue_id):
 #  ----------------------------------------------------------------
 @app.route('/artists')
 def artists():
-    # TODO: replace with real data returned from querying the database
-    data = [{
-        "id": 4,
-        "name": "Guns N Petals",
-    }, {
-        "id": 5,
-        "name": "Matt Quevedo",
-    }, {
-        "id": 6,
-        "name": "The Wild Sax Band",
-    }]
+    data = []
+    all_artists = Artist.query.all()
+
+    for artist in all_artists:
+        data.append({
+            "id": artist.id,
+            "name": artist.name
+        })
     return render_template('pages/artists.html', artists=data)
 
 
